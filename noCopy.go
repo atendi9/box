fe8470d29ceee63390bec83a766718136b030f03
gabrielluizsf <93672097+gabrielluizsf@users.noreply.github.com>
2026-06-10 09:54:59 -0300

    Add noCopy type to prevent struct copying after first use

---
+++ noCopy.go
@@ -0,0 +1,9 @@
+package box
+
+// noCopy may be embedded into structs which must not be copied
+// after the first use.
+type noCopy struct{}
+
+// Lock is a no-op used by go vet to check for copying.
+func (*noCopy) Lock()   {}
+func (*noCopy) Unlock() {}
